bcb8f6a223248705adddda7d45449d839be47264
borebrian <borebrian77@gmail.com>
2019-03-10 17:03:42 +0300

    Initial commit

---
+++ app/src/main/java/com/message/smsreceiver/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.message.smsreceiver
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+
+class MainActivity : AppCompatActivity() {
+    private val requestReceiveSms =2
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        if(ActivityCompat.checkSelfPermission(this,Manifest.permission.RECEIVE_SMS) != PackageManager.PERMISSION_GRANTED){
+
+
+            ActivityCompat.requestPermissions(this,arrayOf(Manifest.permission.RECEIVE_SMS),requestReceiveSms)
+
+
+
+        }
+
+            }
+}
